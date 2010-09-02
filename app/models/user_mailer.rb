0bb47090befc8872a500c45dcf31bb608be9ddfa
Urzuae <urzuae@gmail.com>
2010-09-01 22:07:36 -0500

    Refactor mailer views and seeds

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,22 +1,22 @@
 class UserMailer < ActionMailer::Base
   
   def enrollment_notification(user)
-    @recipients = "#{user.email}"
-    @from = "noreply@scrum.com"
-    @subject = "Confirm account"
-    @body = "Activate you account.\nClick on the following link #{register_url(:confirmation_token => user.confirmation_token)} to activate your account.\nEmail: #{user.email}\nPassword: #{user.password}"
+    recipients  "#{user.email}"
+    from        "noreply@scrum.com"
+    subject     "Confirm account"
+    body        :user => user, :register_url => register_url(:confirmation_token => user.confirmation_token)
   end
 
   def scrum_reminder(user)
-    @recipients = "#{user.email}"
-    @from = "noreply@scrum.com"
-    @subject = "Please add your today's scrum"
-    @body = "You have forgotten to add your tasks for today.\nGo now #{root_url} and fill out your tasks."
+    recipients  "#{user.email}"
+    from        "noreply@scrum.com"
+    subject     "Please add your today's scrum"
+    body        :root => root_url
   end
   
   def daily_report
     projects = Project.all
-    recipients    "urzuae@gmail.com"
+    recipients    "navid@freshout.us"
     from          "noreply@scrum.app"
     subject       "SCRUM daily report"
     body          :projects => projects
@@ -24,10 +24,10 @@ class UserMailer < ActionMailer::Base
   
   def comment_notification(comment)
     user = comment.task.user
-    @recipients = "#{user.email}"
-    @from = "noreply@scrum.app"
-    @subject = "Your task has been commented"
-    @body = "Your task has just been commented.\n\"#{user.name}\" just left a comment on one of your tasks.\n\"#{comment.content}\"\nCheck it out now #{root_url}"
+    recipients  "#{user.email}"
+    from        "noreply@scrum.app"
+    subject     "Your task has been commented"
+    body        :user => user, :comment => comment
   end
   
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
-User.create({ :name => "admin", :email => "admin@localhost.com", :admin => true, :password => "123456"})
+User.create({ :name => "admin", :email => "navid@freshout.ud", :admin => true, :password => "redbullkills"})
